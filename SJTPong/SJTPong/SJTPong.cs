5f1712c59184d1215713cf32b0599ad9e97c9634
Anibal Guanca <Administrador@.(none)>
2011-04-16 18:57:38 -0500

    Diminutas correciones:
    [MOD] Se cambio el uso de variables _stat por variables miembro a la clase
          Barra que indican si el jugador es una AI o no.
    [MOD] Se modifico la variable _direccionball por una varable miembro a la clase
          Pelota que indica su direccion.
    [FIX] Agregado una variabe miembro a barra para saber si ya habia sido
          configurada y no se solto el boton, esto arregla el bug de la multiple
          configuracion por matener el boton apretado.

--- SJTPong/SJTPong/SJTPong.cs
+++ SJTPong/SJTPong/SJTPong.cs
@@ -22,14 +22,9 @@ namespace SJTPongGame
         Barra _player2 = new Barra(780,300);
         Pelota _pelota = new Pelota(400,300);
         TimeSpan _timeballmove;
-        Boolean _stat1 = new Boolean();
-        Boolean _stat2 = new Boolean();
-        String _direccionball;
+
         public SJTPong()
         {
-            _stat1 = false;
-            _stat2 = false;
-            _direccionball = "Right"; 
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             graphics.PreferredBackBufferHeight = 600;
@@ -86,45 +81,60 @@ namespace SJTPongGame
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
-            //Movimiento teclado 1
+            //Movimiento teclado 2
             KeyboardState ks = Keyboard.GetState();
 
-            if (ks.IsKeyDown(Keys.Down) && _player2.Pos.Y<=500 && _stat2 == false)
+            if (ks.IsKeyDown(Keys.Down) && _player2.Pos.Y <= 500 && _player2.AI_player == false)
                 _player2.Pos.Y += 5;
 
-            if (ks.IsKeyDown(Keys.Up) && _player2.Pos.Y >= 0 && _stat2 == false)
+            if (ks.IsKeyDown(Keys.Up) && _player2.Pos.Y >= 0 && _player2.AI_player == false)
                 _player2.Pos.Y -= 5;
 
-            //Movimiento teclado 2
-            if (ks.IsKeyDown(Keys.S) && _player1.Pos.Y <= 500 && _stat1 == false)
+            //Movimiento teclado 1
+            if (ks.IsKeyDown(Keys.S) && _player1.Pos.Y <= 500 && _player1.AI_player == false)
                 _player1.Pos.Y += 5;
 
-            if (ks.IsKeyDown(Keys.W) && _player1.Pos.Y >= 0 && _stat1 == false)
+            if (ks.IsKeyDown(Keys.W) && _player1.Pos.Y >= 0 && _player1.AI_player == false)
                 _player1.Pos.Y -= 5;
 
             //Activa IA de Player 1
-            if (ks.IsKeyDown(Keys.F1))
+            if (ks.IsKeyUp(Keys.F1))
             {
-                if (_stat1 == false)
+                //Priemro me fijo si no está apretada F1, si es así puede volverse a apretar
+                //para cambiar la configuración.
+                _player1.Config_player_pressed = false;
+            }
+            if (ks.IsKeyDown(Keys.F1) && _player1.Config_player_pressed == false)
+            {
+                _player1.Config_player_pressed = true;
+                if (_player1.AI_player == false)
                 {
-                    _stat1 = true;
+                    _player1.AI_player = true;
                 }
                 else
                 {
-                    _stat1 = false;
+                    _player1.AI_player  = false; 
                 }
             }
 
             //Activa IA de Player 2
-            if (ks.IsKeyDown(Keys.F2))
+            if (ks.IsKeyUp(Keys.F2))
             {
-                if (_stat2 == false)
+                //Priemro me fijo si no está apretada F2, si es así puede volverse a apretar
+                //para cambiar la configuración.
+                _player2.Config_player_pressed = false;
+            }
+            if (ks.IsKeyDown(Keys.F2) && _player2.Config_player_pressed == false)
+            {
+                _player2.Config_player_pressed = true;
+
+                if (_player2.AI_player == false)
                 {
-                    _stat2 = true;
+                    _player2.AI_player = true;
                 }
                 else
                 {
-                    _stat2 = false;
+                    _player2.AI_player = false;
                 }
             }
             //Detectar colision de pelota con barra
@@ -132,17 +142,17 @@ namespace SJTPongGame
             Boolean condition_bc_y_p2 = _pelota.Pos.Y + _pelota.Size.Y >= _player2.Pos.Y && _pelota.Pos.Y <= _player2.Pos.Y + _player2.Size.Y;
             Boolean condition_bc_x_p1 = _pelota.Pos.X <= (10 + _player1.Size.X) ;
             Boolean condition_bc_x_p2 = _pelota.Pos.X + _pelota.Size.X >= 790 - _player2.Size.X; 
-            if ((condition_bc_x_p2&&condition_bc_y_p2&&_direccionball=="Right"))
+            if ((condition_bc_x_p2&&condition_bc_y_p2&& _pelota.direccionball=="Right"))
             {
                 _pelota.Velocity.X *= -1;
                 //Fix bug direccion ball sobre barra
-                _direccionball = "Left";
+                _pelota.direccionball = "Left";
             }
-            else if (condition_bc_y_p1 && condition_bc_x_p1 && _direccionball == "Left")
+            else if (condition_bc_y_p1 && condition_bc_x_p1 && _pelota.direccionball == "Left")
             {
                 _pelota.Velocity.X *= -1;
                 //Fix bug direccion ball sobre barra
-                _direccionball = "Right";
+                _pelota.direccionball = "Right";
             }
             else {
                 //Detectar fin de camino y poner en el medio
@@ -150,7 +160,7 @@ namespace SJTPongGame
                 {
                     _pelota.Pos.X = 400;
                     _pelota.Pos.Y = 300;
-                    _direccionball = "Left";
+                    _pelota.direccionball = "Left";
                     //Punto para el lado izquierdo
                 }
 
@@ -158,7 +168,7 @@ namespace SJTPongGame
                 {
                     _pelota.Pos.X = 400;
                     _pelota.Pos.Y = 300;
-                    _direccionball = "Right";
+                    _pelota.direccionball = "Right";
                     //Punto para el lado derecho
                 }
             }
@@ -179,7 +189,7 @@ namespace SJTPongGame
             //IA Player 2
             //Aca practicamente estoy haciendo la AI de la PC
             //Me fijo el centro de la pelota e intento ir hacia alla.
-            if (_stat2 == true)
+            if (_player2.AI_player == true)
             {
                 UInt32 next_move = (UInt32)_pelota.Pos.Y + (UInt32)(_pelota.Size.Y / 2);
 
@@ -193,7 +203,7 @@ namespace SJTPongGame
             }
 
             //IA Player 1
-            if (_stat1 == true)
+            if (_player1.AI_player == true)
             {
                 UInt32 next_move = (UInt32)_pelota.Pos.Y + (UInt32)(_pelota.Size.Y / 2);
 
@@ -231,10 +241,13 @@ namespace SJTPongGame
         public Vector2 Pos = new Vector2(0,0); 
         //Por ahora el tamaño lo pongo hardcodeado
         public Vector2 Size = new Vector2(10, 100);
-
+        public Boolean AI_player;
+        public Boolean Config_player_pressed;
         public Barra(UInt32 A,UInt32 B) 
         {
             Pos.X = A; Pos.Y = B;
+            AI_player = false; //Esto significa que ambos jugadores serán humanos
+            Config_player_pressed = false;//Para eliminar el efecto rebote y no bloquear el programa
         }
         public UInt32 Load_texture(Texture2D textura)
         {
@@ -249,11 +262,12 @@ namespace SJTPongGame
         //Por ahora el tamaño lo pongo hardcodeado
         public Vector2 Size = new Vector2(10, 10);
         public Vector2 Velocity = new Vector2(0,0);
-
+        public String  direccionball;
         public Pelota(UInt32 A=0,UInt32 B=0) 
         {
             Pos.X = A; Pos.Y = B;
             Velocity.X = 5; Velocity.Y = 5;
+            direccionball = "Right";
         }
         public UInt32 Load_texture(Texture2D textura)
         {
